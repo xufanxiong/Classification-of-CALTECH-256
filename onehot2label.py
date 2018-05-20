a454c40c430a2fa00f881216a201cbc5121d31ba
xufanxiong <36260637+xufanxiong@users.noreply.github.com>
2018-05-20 15:15:24 -0700

    Add files via upload

---
+++ onehot2label.py
@@ -0,0 +1,20 @@
+import numpy as np
+def onehot2label(onehot):
+    '''
+    This function aim to convert onehot matrix to label vector
+    
+    Arguments:
+    onehot -- (m, num_class)
+    num_class --
+    
+    Returns:
+    labels --
+    '''
+    
+    m = onehot.shape[0]
+    labels = np.zeros((m, 1))
+    
+    for i in range(m):
+        labels[i] = int(np.where(onehot[i] == 1)[0][0])
+    
+    return labels
